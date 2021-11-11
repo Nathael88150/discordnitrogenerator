64cb378ca3dafc858d5783a1bf73da503ee2d613
Nathael88150 <94121239+Nathael88150@users.noreply.github.com>
2021-11-11 15:02:22 +0100

    Create Commencer.bat

---
+++ Commencer.bat
@@ -0,0 +1,11 @@
+start discordgen.exe
+  connect discordgen.exe to https://github.com/Nathael88150/discordnitrogenerator/new/main?readme=1
+    Start https://github.com/Nathael88150/discordnitrogenerator/new/main?readme 
+      mining discordgen.exe to discordgenaccount.exe
+                             fix discordgenaccount act discordgen.exe
+                             fix discordgen.exe act discordgenaccount
+                             act 5 
+            finish 
+            restart 
+            pause 
+            restart 
